c2bff63fd899014f1fb5c19ebbded0e5b811d508
Olasunkanmi001 <dammyvintage@gmail.com>
2024-11-23 20:25:56 +0100

    add issues

--- src/Component/Issue7.jsx
+++ src/Component/Issue7.jsx
@@ -3,12 +3,12 @@ import image from '../assets/images/Issue_7.png'
 
 const Issue7 = () => {
   return (
-    <div className='grid text-center'>
+    <div className='grid text-center bg-[#FF608C]'>
       <img src={image} className='shadow-2xl'/>
-      <h1>Issue #7</h1>
-      <p>BUY HERE <span>(Europe)</span></p>
-      <p>BUY HERE <span>(UK & Overseas)</span></p>
-      <h6>or in <span>selected store</span></h6>
+      <h1 className='text-black text-lg font-bold mt-5 mb-5'>Issue #7</h1>
+      <p className='text-white font-bold'>BUY HERE <span className='text-black'>(Europe)</span></p>
+      <p className='text-white font-bold mb-5 mt-2'>BUY HERE <span className='text-black'>(UK & Overseas)</span></p>
+      <h6 className='font-bold'>or in <span className='text-white'>selected stores</span></h6>
 
     </div>
   )
